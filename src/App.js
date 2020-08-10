ed656766c4fd4b42ec42ebf5bca7210d01251939
TS22082 <ts22082@gmail.com>
2020-08-10 12:20:05 -0700

    test

--- src/App.js
+++ src/App.js
@@ -5,27 +5,25 @@ import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Form from "./components/Form/Form";
 import Nav from "./components/Nav/Nav";
+import Text from "./components/Text/Text";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Nav />
-        {/* <Link to="/">Home</Link>
+        {/* <Nav /> */}
+        <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link> */}
+        <Link to="/contact">Contact</Link>
 
         <Switch>
           <Route exact path="/">
-            <h1>Hello from Home</h1>
+            <Text message="from home page" />
           </Route>
           <Route exact path="/about">
-            <h1>Hello from About</h1>
+            <Text message="from about page" />
           </Route>
           <Route exact path="/contact" component={Form} />
-          {/* <Route exact path="/contact">
-            <Form />
-          </Route> */}
         </Switch>
       </Router>
     </div>
