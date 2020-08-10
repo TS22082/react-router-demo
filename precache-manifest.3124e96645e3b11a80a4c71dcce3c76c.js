2611143a286b3836e010245c241198be702c2dcb
TS22082 <ts22082@gmail.com>
2020-08-10 12:29:58 -0700

    Updates

---
+++ precache-manifest.3124e96645e3b11a80a4c71dcce3c76c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee0c0f2735013c38df4bdb5a967444f",
+    "url": "/react-router-demo/index.html"
+  },
+  {
+    "revision": "53a0deb0c8f940caae07",
+    "url": "/react-router-demo/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0c5dfe3721bb230264d1",
+    "url": "/react-router-demo/static/js/2.18af3339.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-router-demo/static/js/2.18af3339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a0deb0c8f940caae07",
+    "url": "/react-router-demo/static/js/main.1773874b.chunk.js"
+  },
+  {
+    "revision": "4b6a70bfd56f589cc4cf",
+    "url": "/react-router-demo/static/js/runtime-main.a23f2630.js"
+  }
+]);
